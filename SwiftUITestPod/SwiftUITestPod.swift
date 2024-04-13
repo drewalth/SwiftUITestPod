b5d5b7c6a5a921db900dbb7f4b929161b516313d
drewalth <16584198+drewalth@users.noreply.github.com>
2024-04-12 19:16:40 -0600

    chore: dummy view

--- SwiftUITestPod/SwiftUITestPod.swift
+++ SwiftUITestPod/SwiftUITestPod.swift
@@ -5,6 +5,16 @@
 //  Created by Andrew Althage on 4/12/24.
 //
 
-class SwiftUITestPod {
+import SwiftUI
 
+public struct AwesomeView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello World!")
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
 }
